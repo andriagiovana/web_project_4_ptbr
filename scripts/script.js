b8fc5e2e4ee892696b0eec799c94f6eb72733712
Ândria Cabeça <andriaagio@gmail.com>
2023-07-05 08:09:22 -0300

    projeto 5

--- scripts/script.js
+++ scripts/script.js
@@ -18,32 +18,17 @@ function openPopup() {
   editButton.addEventListener("click" , openPopup);
   closeButton.addEventListener("click" , closePopup); */
 
-    let profile = document.querySelector(".profile");
-    let popup = document.querySelector(".popup");
-    let editButton = document.querySelector('.profile__edit-button');
-    let closeButton = document.getElementById('closeButton');
-    let nameInput = document.getElementById('name');
-    let infoInput = document.getElementById('info');
-    let profileName = document.querySelector('.profile__name_p');
-    let profileInfo = document.querySelector('.profile__text');
-
-    // Exibir o popup quando o botão de edição for clicado
+    /*
     editButton.addEventListener('click', function() {
       popup.style.display = 'block';
       nameInput.value = profileName.textContent;
       infoInput.value = profileInfo.textContent;
-    });
-
-    function togglePopup() {
-      popup.classList.toggle("popup_show");
-    }
-
-    // Fechar o popup quando o botão de fechar for clicado
-    closeButton.addEventListener('click', function() {
+    })
+    
+     closeButton.addEventListener('click', function() {
       popup.style.display = 'none';
     });
 
-    // Atualizar o nome e informações do perfil quando o formulário for enviado
     const editForm = document.getElementById('editForm');
     editForm.addEventListener('submit', function(event) {
       event.preventDefault();
@@ -51,4 +36,34 @@ function openPopup() {
       profileInfo.textContent = infoInput.value;
       popup.style.display = 'none';
     });
+    
+    */ 
+
+    let profile = document.querySelector(".profile");
+    let popup = document.querySelector(".popup");
+    let editButton = document.querySelector('.profile__edit-button');
+    let addButton = document.querySelector('.profile__add-button');
+    let closeButton = document.querySelector(".close-icon");
+    let nameInput = document.getElementById('name');
+    let infoInput = document.getElementById('info');
+    let profileName = document.querySelector('.profile__name_p');
+    let profileInfo = document.querySelector('.profile__text');
+
+
+    function togglePopup() {
+      popup.classList.toggle("popup_show");
+    }
+
+    editButton.addEventListener("click" , togglePopup);
+    closeButton.addEventListener("click" , togglePopup);
+
+   
+    const editForm = document.getElementById('editForm');
+    editForm.addEventListener('submit', function(event) {
+      event.preventDefault();
+      profileName.textContent = nameInput.value;
+      profileInfo.textContent = infoInput.value;
+      popup.style.display = 'none';
+    });
+    
   
